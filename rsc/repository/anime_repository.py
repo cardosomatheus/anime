6bca8f03c67f41e0432b2b467d6b058cbdbdbc30
marilan <cardosomcds1@gmail.com>
2025-11-23 15:29:48 -0300

    Definindo a conexão como param de inicializacao.

--- rsc/repository/anime_repository.py
+++ rsc/repository/anime_repository.py
@@ -1,30 +1,27 @@
-from interface.anime_interface import Ianime
-from db.database import ConexaoDB
-from ..model.anime_model import AnimeModel
 from sqlalchemy import select, delete, update
+from sqlalchemy.orm import Session
 from datetime import date
+
+from interface.anime_interface import Ianime
+from ..model.anime_model import AnimeModel
 
 
 class RepositoryAnime(Ianime):
-    def __init__(self) -> None:
-        pass
+    def __init__(self, session: Session) -> None:
+        self.session = session
 
     def cria_anime(self,
                    nome: str,
                    data_lancamento: date,
                    descricao: str,
-                   db: ConexaoDB) -> AnimeModel:
-        """_summary_
-
+                   ) -> AnimeModel:
+        """
         Args:
             nome (str): Nome do anime
             data_lancamento (date): data de lancamento
             descricao (str): Descricao do anime
-            db (ConexaoDB): Class ConexaoDB para acessarmos o banco de dados.
-
         Raises:
             Exception: Erro no cadastro
-
         Returns:
             AnimeModel: Retorna o model da tabela.
         """
@@ -36,7 +33,7 @@ class RepositoryAnime(Ianime):
                                      data_lancamento=data_lancamento,
                                      descricao=descricao)
 
-        with db.session() as conn:
+        with self.session as conn:
             try:
                 conn.add(new_anime_model)
                 conn.commit()
@@ -48,12 +45,10 @@ class RepositoryAnime(Ianime):
 
         return new_anime_model
 
-    def busca_anime_by_id(self, id: int, db: ConexaoDB) -> str:
-        """_summary_
+    def busca_anime_by_id(self, id: int) -> list:
+        """
         Args:
             id (int): Buscaremos o ANIME pelo ID
-            db (ConexaoDB): Class ConexaoDB para acessarmos o banco de dados.
-
         Raises:
             Exception: Se o ID não existir, retornamos None
 
@@ -63,24 +58,20 @@ class RepositoryAnime(Ianime):
         if id is not None and not isinstance(id, int):
             raise Exception('ID deve ser uma valor inteiro!!')
 
-        with db.session() as conn:
+        with self.session as conn:
             try:
                 query = select(AnimeModel.nome).where(AnimeModel.id == id)
                 return conn.execute(query).first()
 
             except TypeError:
-                return None
+                return [None]
 
-    def busca_all_animes(self, db: ConexaoDB) -> list:
-        """_summary_
-
-        Args:
-            db (ConexaoDB): Class ConexaoDB para acessarmos o banco de dados.
-
+    def busca_all_animes(self) -> list:
+        """
         Returns:
             list: Retornamos em formato de lista todos os animes.
         """
-        with db.session() as conn:
+        with self.session as conn:
             try:
                 query = select(
                                AnimeModel.id,
@@ -93,13 +84,10 @@ class RepositoryAnime(Ianime):
             except Exception:
                 return []
 
-    def deleta_anime(self, id: int,  db: ConexaoDB) -> None:
+    def deleta_anime(self, id: int) -> bool:
         """Faz a deleção do anime pelo ID
-
         Args:
             id (int): id do anime
-            db (ConexaoDB): Class ConexaoDB para acessarmos o banco de dados.
-
         Raises:
             Exception: ID deve ser inteiro
             Exception: ID não deve ser Nulo
@@ -110,27 +98,24 @@ class RepositoryAnime(Ianime):
         if id is None:
             raise Exception('Passe o ID (valor Inteiro)')
 
-        with db.session() as conn:
+        with self.session as conn:
             try:
                 query = delete(AnimeModel).where(AnimeModel.id == id)
                 conn.execute(query)
                 conn.commit()
+                return True
             except Exception as error:
                 print(error)
+                return False
 
     def atualiza_anime(self,
                        id: int,
-                       dict_Anime_model: dict,
-                       db: ConexaoDB) -> AnimeModel:
+                       dict_Anime_model: dict) -> AnimeModel:
         """Atualizamos campos contidos no model de anime
         Args:
             id (int): id do anime
-            dict_Anime_model (dict): Os campos a ser atualizado e o novos valores.
-            db (ConexaoDB): Class ConexaoDB para acessarmos o banco de dados.
-
         Raises:
             Exception: ID deve ser inteiro
-
         Returns:
             AnimeModel: _description_
         """
@@ -141,7 +126,7 @@ class RepositoryAnime(Ianime):
             key: value for key, value in dict_Anime_model.items()
         }
 
-        with db.session() as conn:
+        with self.session as conn:
             try:
                 query = update(AnimeModel)\
                         .where(AnimeModel.id == id)\
@@ -154,21 +139,23 @@ class RepositoryAnime(Ianime):
 
 
 if __name__ == "__main__":
-    myrepo = RepositoryAnime()
-    con_db = ConexaoDB()
-    print(myrepo.busca_anime_by_id(13, con_db))
+    from db.database import ConexaoDB
+
+    con_db = ConexaoDB().session()
+    myrepo = RepositoryAnime(con_db)
     dddd = {'nome': 'Leviathan', 'idade': '19'}
-    myrepo.atualiza_anime(13, dddd, con_db)
-    print(myrepo.busca_anime_by_id(13, con_db))
-    myrepo.delete_anime(11, con_db)
-    print(myrepo.busca_all_animes(con_db))
-    myrepo.cria_anime(
-        nome='Leviathan31',
-        data_lancamento=date(day=10, month=7, year=2025),
-        descricao="""É uma série steampunk adaptada do livro de Scott
-        Westerfeld. A história se passa num universo alternativo para 1914,
-        onde existem criaturas vivas (como navios dirigidos) e máquinas
-        biológicas. Dois protagonistas — um príncipe (Aleksandar) e uma
-        garota disfarçada de menino (Deryn)""",
-        db=con_db
-    )
+
+    print(myrepo.busca_anime_by_id(13))
+    print(myrepo.busca_all_animes())
+    myrepo.atualiza_anime(13, dddd)
+    myrepo.deleta_anime(11)
+
+#    myrepo.cria_anime(
+#        nome='Leviathan31',
+#        data_lancamento=date(day=10, month=7, year=2025),
+#        descricao="""É uma série steampunk adaptada do livro de Scott
+#        Westerfeld. A história se passa num universo alternativo para 1914,
+#        onde existem criaturas vivas (como navios dirigidos) e máquinas
+#        biológicas. Dois protagonistas — um príncipe (Aleksandar) e uma
+#        garota disfarçada de menino (Deryn)"""
+#    )
